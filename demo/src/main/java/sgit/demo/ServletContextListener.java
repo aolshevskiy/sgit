6513ae772c4d860fe9ae9a0c2aeb76c7b300826a
Artyom Olshevskiy <siasiamail@gmail.com>
2012-01-08 18:10:51 +0300

    Aggregator project

--- demo/src/main/java/sgit/demo/ServletContextListener.java
+++ demo/src/main/java/sgit/demo/ServletContextListener.java
@@ -2,13 +2,14 @@ package sgit.demo;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import com.google.inject.Stage;
 import com.google.inject.servlet.GuiceServletContextListener;
 
 public class ServletContextListener extends GuiceServletContextListener {
 
 	@Override
 	protected Injector getInjector() {
-		return Guice.createInjector(new SGitModule());
+		return Guice.createInjector(Stage.PRODUCTION, new SGitModule());
 	}
 
 }
